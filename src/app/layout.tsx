66a3dfcaeeb94b4eceb480c4497a351f6a42eaff
Miracle Friday <miraclef60@gmail.com>
2024-06-13 15:17:32 +0100

    fix: fixed layout and added icon re-routing to home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,9 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider, ThemeToggle } from "@/components/theme-provider";
 import { cn } from "@/lib/utils";
+import Link from "next/link";
+import Image from "next/image";
+import icon from "@/app/icon.png";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -37,7 +40,7 @@ export default function RootLayout({
 function Header() {
   return (
     <header className="container max-h-20 px-3 py-3 md:py-6 md:px-6 mx-auto rounded-lg flex place-items-center items-center justify-between">
-      <span>FlintAPI</span>
+      <Link href="/" className="font-[600] tracking-wide text-[14px] text-black dark:text-white flex items-center gap-2"><Image src={icon} width={22} className="border-[.5px] border-black/40 dark:border-slate-300/40 rounded-md" height={22} alt="Flintapi Logo" />  FlintAPI</Link>
       <ThemeToggle />
     </header>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import convexityLogo from "@/assets/convexity_logo_thumb.png";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center items-center px-16 md:px-24 py-10">
+    <main className="flex h-[calc(100vh-80px)] flex-col items-center items-center px-16 md:px-24 py-10">
 
       <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[280px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-orange-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-orange-700 before:dark:opacity-10 after:dark:from-[#EE4751] after:dark:via-[#EDF128] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
         <h1 className="sm:text-6xl text-[2.5em] leading-tight text-center font-semibold">API Infrastructure for<br className="hidden md:block" /> nextgen builders</h1>
@@ -20,7 +21,7 @@ export default function Home() {
 
       <div className="flex-1" />
       <div className="flex place-items-center flex-1 md:flex-none">
-        <span className="font-light">Backed by <a href="https://withconvexity.com" rel="noopener noreferer" target="_blank" className="text-sky-500">Convexity</a></span>
+        <span className="font-light flex items-center gap-2">Backed by <a href="https://withconvexity.com" rel="noopener noreferer" target="_blank" className="text-[#2853BB] flex items-center gap-1 font-normal"><Image src={convexityLogo} height={20} width={20} alt="Convexity logo thumb" />Convexity</a></span>
       </div>
 
     </main>
