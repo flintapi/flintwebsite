2e57feaace92beac0030c11b66b442b9151e99f4
Miracle Friday <miraclef60@gmail.com>
2024-06-13 15:54:02 +0100

    fix: fixed layout and page files
    - Added dialog component from shadcn

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider, ThemeToggle } from "@/components/theme-provider";
@@ -12,6 +12,13 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "FlintAPI",
   description: "Simplifying next gen infrastructure consumption simple API conventions",
+};
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 0.7,
+  maximumScale: 1,
+  userScalable: false,
 };
 
 export default function RootLayout({
@@ -39,7 +46,7 @@ export default function RootLayout({
 
 function Header() {
   return (
-    <header className="container max-h-20 px-3 py-3 md:py-6 md:px-6 mx-auto rounded-lg flex place-items-center items-center justify-between">
+    <header className="max-h-20 px-8 py-4 md:py-6 md:px-20 lg:px-40 mx-auto rounded-lg flex place-items-center items-center justify-between">
       <Link href="/" className="font-[600] tracking-wide text-[14px] text-black dark:text-white flex items-center gap-2"><Image src={icon} width={22} className="border-[.5px] border-black/40 dark:border-slate-300/40 rounded-md" height={22} alt="Flintapi Logo" />  FlintAPI</Link>
       <ThemeToggle />
     </header>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,9 @@ import convexityLogo from "@/assets/convexity_logo_thumb.png";
 
 export default function Home() {
   return (
-    <main className="flex h-[calc(100vh-80px)] flex-col items-center items-center px-16 md:px-24 py-10">
+    <main className="flex h-[calc(100vh-80px)] flex-col items-center items-center place-items-center px-6 md:px-24 py-20">
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[280px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-orange-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-orange-700 before:dark:opacity-10 after:dark:from-[#EE4751] after:dark:via-[#EDF128] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
+      <div className="relative md:mt-0 mt-20 z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-10 after:w-10 md:after:w-[280px] md:after:h-[280px] after:translate-x-1/3 after:bg-gradient-conic after:from-orange-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-orange-700 before:dark:opacity-10 after:dark:from-[#EE4751] after:dark:via-[#EDF128] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
         <h1 className="sm:text-6xl text-[2.5em] leading-tight text-center font-semibold">API Infrastructure for<br className="hidden md:block" /> nextgen builders</h1>
       </div>
 
@@ -15,13 +15,13 @@ export default function Home() {
         <span className="text-center font-light text-[1.2rem] md:text-lg">Focus on your solutions and faster market entry, supported by <br className="hidden md:block" /> reliable and highly efficient infrastructure.</span>
       </div>
 
-      <div className="flex place-items-center mt-5">
+      <div className="flex place-items-center mt-8">
         <Button size="lg">Join the waitlist</Button>
       </div>
 
       <div className="flex-1" />
       <div className="flex place-items-center flex-1 md:flex-none">
-        <span className="font-light flex items-center gap-2">Backed by <a href="https://withconvexity.com" rel="noopener noreferer" target="_blank" className="text-[#2853BB] flex items-center gap-1 font-normal"><Image src={convexityLogo} height={20} width={20} alt="Convexity logo thumb" />Convexity</a></span>
+        <span className="font-light flex items-center text-sm gap-2">Backed by <a href="https://withconvexity.com" rel="noopener noreferer" target="_blank" className="text-[#2853BB] flex items-center gap-1 font-normal"><Image src={convexityLogo} height={20} width={20} alt="Convexity logo thumb" />Convexity</a></span>
       </div>
 
     </main>
