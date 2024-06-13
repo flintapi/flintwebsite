bb536df84d0caa1119e275649fe4218195893176
Miracle Friday <miraclef60@gmail.com>
2024-06-13 13:20:35 +0100

    fix: fixed entry page and type

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider, ThemeToggle } from "@/components/theme-provider";
+import { cn } from "@/lib/utils";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +18,27 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={cn(inter.className)}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Header />
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
+  );
+}
+
+
+function Header() {
+  return (
+    <header className="container max-h-20 px-3 py-3 md:py-6 md:px-6 mx-auto rounded-lg flex place-items-center items-center justify-between">
+      <span>FlintAPI</span>
+      <ThemeToggle />
+    </header>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
@@ -5,14 +6,17 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center items-center px-16 md:px-24 py-10">
 
       <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[280px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-orange-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-orange-700 before:dark:opacity-10 after:dark:from-[#EE4751] after:dark:via-[#EDF128] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <h1 className="sm:text-6xl text-[2.5em] leading-tight text-center font-semibold">API Infrastructure tools for<br className="hidden md:block" /> nextgen builders</h1>
+        <h1 className="sm:text-6xl text-[2.5em] leading-tight text-center font-semibold">API Infrastructure for<br className="hidden md:block" /> nextgen builders</h1>
       </div>
 
 
       <div className="flex place-items-center mt-5">
-        <span className="text-center font-light text-[1.2rem] md:text-lg">Focus on solutions and reducing time to market, with the confidence <br className="hidden md:block"/> of building on reliable and highly available infrastructure.</span>
+        <span className="text-center font-light text-[1.2rem] md:text-lg">Focus on your solutions and faster market entry, supported by <br className="hidden md:block" /> reliable and highly efficient infrastructure.</span>
       </div>
 
+      <div className="flex place-items-center mt-5">
+        <Button size="lg">Join the waitlist</Button>
+      </div>
 
       <div className="flex-1" />
       <div className="flex place-items-center flex-1 md:flex-none">
