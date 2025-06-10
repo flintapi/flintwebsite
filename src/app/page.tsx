2aea5fa09ad6fc4889629ba45eaf8a43e042f3b4
Miracle Friday <miraclef60@gmail.com>
2025-06-10 07:46:48 +0100

    refactor: Update hero tag line

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,8 +8,8 @@ export default function Home() {
       <div className="mt-[20vh]" />
       <div className="relative md:mt-0 mt-20 z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-10 after:w-10 md:after:w-[280px] md:after:h-[280px] after:translate-x-1/3 after:bg-gradient-conic after:from-orange-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-orange-700 before:dark:opacity-10 after:dark:from-[#EE4751] after:dark:via-[#EDF128] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
         <h1 className="sm:text-6xl text-[2.5em] leading-tight text-center font-semibold">
-          API Infrastructure for
-          <br className="hidden md:block" /> nextgen builders
+          API-first Infrastructure for
+          <br className="hidden md:block" /> Stablecoins
         </h1>
       </div>
 
